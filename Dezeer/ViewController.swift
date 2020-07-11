200c9cfa1d32ee559312df156f821a2b8e6ba811
Ivan Levin <Levin_I@utkonos.ru>
2020-07-11 17:13:52 +0300

    Initial commit

---
+++ Dezeer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dezeer
+//
+//  Created by Levin Ivan on 11.07.2020.
+//  Copyright © 2020 Levin Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
